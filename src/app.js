eb8e9f39a0ad3b2b077f66fcf441a325a485c19d
Jevgenij Dmitrijev <jdmitrijev@users.noreply.github.com>
2019-02-04 17:32:01 +0200

    Added CoreUi Starter logics + webpack config

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Starter from "./js/Starter";
+
+ReactDOM.render(<Starter/>, document.getElementById('app'));
---
+++ src/js/Starter.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+export default class Starter extends Component {
+    render() {
+
+        return <div>Hello React</div>;
+    }
+}
