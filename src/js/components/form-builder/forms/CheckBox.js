f2b46b75f35fe3b63bc5788c5a686498f6fa6f66
Jevgenij Dmitrijev <jdmitrijev@users.noreply.github.com>
2019-02-18 14:43:19 +0200

    Added check box field

---
+++ src/js/components/form-builder/forms/CheckBox.js
@@ -0,0 +1,38 @@
+import React from "react";
+import * as PropTypes from "prop-types";
+import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox/Checkbox";
+import BaseField from "./BaseField";
+
+export default class CheckBox extends BaseField {
+
+    render() {
+        return <FormControlLabel
+            label={this.props.label}
+            control={
+                <Checkbox value={this.props.id}
+                          color="primary"
+                          defaultChecked={this.props.defaultValue}
+                          onChange={(e) => {
+                              this.props.onChange(this.props.id, e.target.checked);
+                          }}/>
+            }
+        />
+    }
+}
+
+CheckBox.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    formId: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.bool,
+    defaultValue: PropTypes.bool,
+    annotation: PropTypes.string,
+    required: PropTypes.bool
+};
+
+CheckBox.defaultProps = {
+    required: false,
+    value: true,
+};
