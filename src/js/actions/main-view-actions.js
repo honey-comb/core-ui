b20903d81ea2fbe72ad5e2d4492001bd3d9576ef
Jevgenij Dmitrijev <jdmitrijev@users.noreply.github.com>
2019-02-18 14:54:04 +0200

    Added main view actions

--- src/js/actions/main-view-actions.js
+++ src/js/actions/main-view-actions.js
@@ -1,11 +1,40 @@
-export const CHANGE_MAIN_VIEW = 'main-view:change';
+import axios from "axios";
+import {Configuration} from "../helpers/Configuration";
 
-export function changeMainView (view)
-{
+export const CHANGE_MAIN_VIEW = 'main-view:change';
+export const VIEW_LOGIN = 'main-view:login';
+
+/**
+ * Change main view
+ *
+ * @param view
+ * @returns {{type: string, payload: {view: *}}}
+ */
+export function changeMainView(view) {
     return {
         type: CHANGE_MAIN_VIEW,
         payload: {
             view: view
         }
     }
+}
+
+/**
+ * Load Configuration
+ *
+ * @param url
+ * @returns {function(*): Promise<AxiosResponse<any> | never>}
+ */
+export function loadConfig(url) {
+
+    return dispatch => {
+
+        return axios.get(url)
+            .then(({data}) => {
+
+                Configuration.data = data;
+
+                dispatch(changeMainView(VIEW_LOGIN));
+            });
+    };
 }
