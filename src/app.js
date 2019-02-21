b5d98f8a90f3dabe8de7cefef194f848bc37069a
Jevgenij Dmitrijev <jdmitrijev@users.noreply.github.com>
2019-02-21 12:27:55 +0200

    Registering form fields and Reducers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import Starter from "./js/MainView";
+import MainView from "./js/MainView";
 
 import {Provider} from "react-redux";
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import MainViewReducer from "./js/reducers/main-view-reducer";
+import FormReducer from "./js/reducers/form-builder-reducer";
+import FieldsReducer from "./js/reducers/fields-reducer";
 import thunk from "redux-thunk";
 
+import {Configuration} from "./js/helpers/Configuration";
+import SingleLine from "./js/components/form-builder/forms/SingleLine";
+import Password from "./js/components/form-builder/forms/Password";
+import Email from "./js/components/form-builder/forms/Email";
+import CheckBox from "./js/components/form-builder/forms/CheckBox";
+
+/**
+ * Registering available form fields
+ */
+Configuration.formFields.register('singleLine', SingleLine);
+Configuration.formFields.register('password', Password);
+Configuration.formFields.register('email', Email);
+Configuration.formFields.register('checkBox', CheckBox);
+
+/**
+ * Registering reducers
+ * @type {Reducer<any> | Reducer<any, AnyAction>}
+ */
 const rootReducer = combineReducers({
     mainView: MainViewReducer,
+    formBuilder: FormReducer,
+    fieldValues: FieldsReducer
 });
 
 /**
