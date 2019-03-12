25c101387cbc365cf356275e5484d712e863fc74
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2019-03-12 13:24:45 +0200

    Refactored app.js

--- src/app.js
+++ src/app.js
@@ -1,55 +1,46 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import MainView from "./js/MainView";
+import {Globals} from "./js/helpers/Globals";
 
-import {Provider} from "react-redux";
-import {applyMiddleware, combineReducers, compose, createStore} from "redux";
-import MainViewReducer from "./js/reducers/main-view-reducer";
-import FormReducer from "./js/reducers/form-builder-reducer";
-import FieldsReducer from "./js/reducers/fields-reducer";
-import thunk from "redux-thunk";
-
-import {Configuration} from "./js/helpers/Configuration";
 import SingleLine from "./js/components/form-builder/forms/SingleLine";
 import Password from "./js/components/form-builder/forms/Password";
 import Email from "./js/components/form-builder/forms/Email";
 import CheckBox from "./js/components/form-builder/forms/CheckBox";
 
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {Route, Router} from 'react-router'
+import configureStore from "./js/MainStore";
+import {createBrowserHistory} from "history";
+import HomeView from "./js/components/views/HomeView";
+
+//TODO find better place for form field register
 /**
  * Registering available form fields
  */
-Configuration.formFields.register('singleLine', SingleLine);
-Configuration.formFields.register('password', Password);
-Configuration.formFields.register('email', Email);
-Configuration.formFields.register('checkBox', CheckBox);
+Globals.formFields.register('singleLine', SingleLine);
+Globals.formFields.register('password', Password);
+Globals.formFields.register('email', Email);
+Globals.formFields.register('checkBox', CheckBox);
 
 /**
- * Registering reducers
- * @type {Reducer<any> | Reducer<any, AnyAction>}
+ * Initializing browser history
  */
-const rootReducer = combineReducers({
-    mainView: MainViewReducer,
-    formBuilder: FormReducer,
-    fieldValues: FieldsReducer
-});
+export const history = createBrowserHistory();
 
-/**
- * Registering store enhancers
- */
-const composedEnhancers = compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+Globals.render = function () {
+    const store = configureStore();
 
-/**
- * Creating Redux Store
- */
-const store = createStore(rootReducer, {}, composedEnhancers);
+    /**
+     * Placing Item on stage
+     **/
+    ReactDOM.render(
+        <Provider store={store}>
+            <Router history={history}>
+                <Route path={'*'} exact={true} component={HomeView}/>
+            </Router>
+        </Provider>,
+        document.getElementById('app')
+    );
+};
 
-/**
- * Placing Item on stage
- */
-ReactDOM.render(
-    <Provider store={store}>
-        <MainView randomProps={"whatever"} configUrl={document.querySelector('#app').dataset.configUrl}/>
-    </Provider>, document.getElementById('app'));
+Globals.config.init(document.querySelector('#app').dataset.configUrl);
