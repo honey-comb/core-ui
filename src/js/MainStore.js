e03ca4f6e49c6c3c9383f5736e397a2bf66261a3
Jevgenij Dmitrijev <jevgenijdmitrijev@Jevgenijs-MacBook-Pro.local>
2019-03-26 12:32:31 +0200

    upgraded initialization

--- src/js/MainStore.js
+++ src/js/MainStore.js
@@ -1,16 +1,21 @@
-import { applyMiddleware, compose, createStore } from 'redux'
+import {applyMiddleware, compose, createStore} from 'redux'
 import createRootReducer from './MainReducer'
 import thunk from "redux-thunk";
+
+let composeData = [];
+
+composeData.push(applyMiddleware(
+    thunk
+));
+
+if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+    composeData.push(window.__REDUX_DEVTOOLS_EXTENSION__());
+}
 
 export default function configureStore(preloadedState) {
     return createStore(
         createRootReducer(history), // root reducer with router state
         preloadedState,
-        compose(
-            applyMiddleware(
-                thunk
-            ),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        ),
+        compose(...composeData),
     );
 }
